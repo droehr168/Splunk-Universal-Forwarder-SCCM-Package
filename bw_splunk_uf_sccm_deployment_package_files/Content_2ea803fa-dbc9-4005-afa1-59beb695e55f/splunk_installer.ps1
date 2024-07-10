b183a139411816d73bb1c081bfaaeeb903cee213
Denis Roehr <100361693+droehr168@users.noreply.github.com>
2024-07-10 22:00:48 +0200

    Update splunk_installer.ps1
    change apps and msi detection

--- bw_splunk_uf_sccm_deployment_package_files/Content_2ea803fa-dbc9-4005-afa1-59beb695e55f/splunk_installer.ps1
+++ bw_splunk_uf_sccm_deployment_package_files/Content_2ea803fa-dbc9-4005-afa1-59beb695e55f/splunk_installer.ps1
@@ -3,23 +3,24 @@
 	===========================================================================
 	 Created with: 	PowerShell Studio
 	 Created on:   	06.07.2024 13:51
-	 Created by:   	drohr@splunk.com
+	 Created by:   	drohr@splunk
 	 Organization: 	splunk
 	 Filename:     	-
 
-     Customer:      XXX
+     Customer:      Bundeswehr IT Security & Audit
 	===========================================================================
 	.DESCRIPTION
-       - install splunk forwarder with random complex password (20 char) 
-	   - write separate logfile under \var\log\splunk\splunk_migration.log
+       - install splunk forwarder with random complex password (32 char) 		
        - remove system/local files
-       - copy deployment server configuration to app folder only
+       - e.g. copy deployment server configuration to app folder only
             * deploymentclient.conf
-            
+       - multiple apps to copy will be automatic detected
+
        - detection of system local files (deletion)
             * deploymentclient.conf
             * outputs.conf
 #>
+
 
 # Function to create a random password
 Function Create-String([Int]$Size = 8, [Char[]]$CharSets = "ULNS", [Char[]]$Exclude) 
@@ -43,9 +44,12 @@ Function Create-String([Int]$Size = 8, [Char[]]$CharSets = "ULNS", [Char[]]$Excl
 }; Set-Alias Create-Password Create-String -Description "Generate a random string (password)"
 
 
+$splunk_process_running_folder = (Get-Process -Name splunkd -FileVersionInfo).FileName
+$splunk_root_directory         = $splunk_process_running_folder.Replace("\bin\splunkd.exe","")
+
 Function Write-InstallerLog([string]$InstallerLog)
     {
-    Write-Output "$InstallerLog" | Out-File -FilePath "C:\Program Files\SplunkUniversalForwarder\var\log\splunk\splunk_migration.log" -Append
+    Write-Output "$InstallerLog" | Out-File -FilePath "$splunk_root_directory\var\log\splunk\splunk_migration.log" -Append
     }
 
 
@@ -54,29 +58,26 @@ Write-InstallerLog "$DateTimeNow INFO update process pre-check Splunk Universal 
 # check if splunkd is running
 $splunk_file_version           = (Get-Process -Name splunkd -FileVersionInfo).FileVersion
 $splunk_process_running        = Get-Process -Name splunkd -ErrorAction SilentlyContinue
-$splunk_process_running_folder = (Get-Process -Name splunkd -FileVersionInfo).FileName
-$splunk_root_directory         = $splunk_process_running_folder.Replace("\bin\splunkd.exe","")
 $check_system_local_conf_files = Get-ChildItem -Path "$splunk_root_directory\etc\system\local"
-$Splunk_Resource_Install_MSI   = "$PSScriptRoot\splunkforwarder.msi"
-$Splunk_Resource_DeployApp     = "$PSScriptRoot\splunk_bw_all_secure_deployment_client"
+$Splunk_Resource_Install_MSI   = (Get-ChildItem -Path $PSScriptRoot -Filter "*.msi").FullName
+$Splunk_Resource_DeployApps    = (Get-ChildItem -Path $PSScriptRoot | ? {$_.Attributes -match "Directory"}).FullName
 $randompwd                     = Create-String 20 ULNS
+
 
 $DateTimeNow = (get-date).tostring("yyyy.MM.dd hh:mm:ss.ffffff tt K", $enus)
 Write-InstallerLog "$DateTimeNow INFO update process pre-check Splunk Universal Forwarder installed version $splunk_file_version"
 Write-InstallerLog "$DateTimeNow INFO update process pre-check Splunk Universal Forwarder installed in $splunk_root_directory"
 Write-InstallerLog "$DateTimeNow INFO update process pre-check Splunk Universal Forwarder MSI --> $Splunk_Resource_Install_MSI"
-Write-InstallerLog "$DateTimeNow INFO update process pre-check Splunk Universal Forwarder DeploymentClient App --> $Splunk_Resource_DeployApp"
-
 
 
 if( ($check_system_local_conf_files.Name -cmatch "deployment") -or ($check_system_local_conf_files.Name -cmatch "output") )
         {
         $DateTimeNow = (get-date).tostring("yyyy.MM.dd hh:mm:ss.ffffff tt K", $enus)
-        Write-InstallerLog "$DateTimeNow ERROR update process pre-check Splunk Universal Forwarder local system files are detected"
-        Write-InstallerLog "$DateTimeNow ERROR update process pre-check Splunk Universal Forwarder local system conf for deploymentclient or outputs detected"
+        Write-InstallerLog "$DateTimeNow INFO update process pre-check Splunk Universal Forwarder error local system files are detected"
+        Write-InstallerLog "$DateTimeNow INFO update process pre-check Splunk Universal Forwarder error local system conf for deploymentclient or outputs detected"
         $delete_deploymentclient = Get-ChildItem -Path "$splunk_root_directory\etc\system\local\deploymentclient.conf" | Remove-Item -Force -ErrorAction SilentlyContinue
         $delete_outputs          = Get-ChildItem -Path "$splunk_root_directory\etc\system\local\outputs.conf" | Remove-Item -Force -ErrorAction SilentlyContinue
-        Write-InstallerLog "$DateTimeNow WARN update process pre-check Splunk Universal Forwarder local system conf for deploymentclient or outputs were deleted"
+        Write-InstallerLog "$DateTimeNow INFO update process pre-check Splunk Universal Forwarder error local system conf for deploymentclient or outputs were deleted"
         }
 
 $DateTimeNow = (get-date).tostring("yyyy.MM.dd hh:mm:ss.ffffff tt K", $enus)
@@ -84,30 +85,27 @@ Write-InstallerLog "$DateTimeNow INFO update process start upgrade splunk forwar
 
 Start-Process -FilePath "C:\Windows\system32\msiexec.exe" -Wait -NoNewWindow -ArgumentList "/i $Splunk_Resource_Install_MSI AGREETOLICENSE=Yes SPLUNKUSERNAME=splunkd SPLUNKPASSWORD=$randompwd /q"
 
-$DateTimeNow = (get-date).tostring("yyyy.MM.dd hh:mm:ss.ffffff tt K", $enus)
-Write-InstallerLog "$DateTimeNow INFO update process copy deploymentclient app $Splunk_Resource_DeployApp to $splunk_root_directory\etc\apps"
-Copy-Item -Path $Splunk_Resource_DeployApp -Destination "$splunk_root_directory\etc\apps" -Recurse -Force -ErrorAction SilentlyContinue
-
-$DateTimeNow = (get-date).tostring("yyyy.MM.dd hh:mm:ss.ffffff tt K", $enus)
-Write-InstallerLog "$DateTimeNow INFO update process restart splunkd"
-Start-Process -FilePath "$splunk_root_directory\bin\splunk.exe" -ArgumentList restart -NoNewWindow -Wait
-
-$DateTimeNow = (get-date).tostring("yyyy.MM.dd hh:mm:ss.ffffff tt K", $enus)
-Write-InstallerLog "$DateTimeNow INFO update process done"
-
-$splunk_file_version_new           = (Get-Process -Name splunkd -FileVersionInfo).FileVersion
-$splunk_process_running_new        = Get-Process -Name splunkd -ErrorAction SilentlyContinue
-
-$DateTimeNow = (get-date).tostring("yyyy.MM.dd hh:mm:ss.ffffff tt K", $enus)
-Write-InstallerLog "$DateTimeNow INFO update process done with new Universal Forwarder Version $splunk_file_version_new"
-if($splunk_process_running_new)
+# copy all required apps to the splunk uf apps folder
+if($Splunk_Resource_DeployApps.Count -gt "1")
     {
-    $DateTimeNow = (get-date).tostring("yyyy.MM.dd hh:mm:ss.ffffff tt K", $enus)
-    Write-InstallerLog "$DateTimeNow INFO update process done with new Universal Forwarder up and running"
-    Write-InstallerLog "$DateTimeNow INFO update process successful"
+
+        foreach($App IN $Splunk_Resource_DeployApps)
+
+        {
+            $DateTimeNow = (get-date).tostring("yyyy.MM.dd hh:mm:ss.ffffff tt K", $enus)
+            Write-InstallerLog "$DateTimeNow INFO update process copy $App to $splunk_root_directory\etc\apps"
+            Copy-Item -Path $App -Destination "$splunk_root_directory\etc\apps" -Recurse -Force -ErrorAction SilentlyContinue
+        }
     }
 else
     {
-    $DateTimeNow = (get-date).tostring("yyyy.MM.dd hh:mm:ss.ffffff tt K", $enus)
-    Write-InstallerLog "$DateTimeNow ERROR update process done but Universal Forwarder is not up and running"
+        $DateTimeNow = (get-date).tostring("yyyy.MM.dd hh:mm:ss.ffffff tt K", $enus)
+        Write-InstallerLog "$DateTimeNow INFO update process copy $ApplicationTransfer to $splunk_root_directory\etc\apps"
+        Copy-Item -Path $Splunk_Resource_DeployApps -Destination "$splunk_root_directory\etc\apps" -Recurse -Force -ErrorAction SilentlyContinue   
     }
+
+
+
+$DateTimeNow = (get-date).tostring("yyyy.MM.dd hh:mm:ss.ffffff tt K", $enus)
+Write-InstallerLog "$DateTimeNow INFO update process restart splunkd"
+Start-Process -FilePath "$splunk_root_directory\bin\splunk.exe" -ArgumentList restart -NoNewWindow
